51ae52c27d841c2cd213159328b8e4f8bfaf03a2
superlpx <lipxiang@foxmail.com>
2020-09-25 21:05:53 +0800

    hlnmsl

--- src/main/webapp/js/login.js
+++ src/main/webapp/js/login.js
@@ -3,14 +3,11 @@ function login(){
     var name = document.getElementById("UserName").value;
     var pwd = document.getElementById("Password").value;
 
-    if(name == null || name.length != 8 || name.length != 13){
+    if(name == null || (name.length != 8 && name.length != 13)){
         alert("用户名错误");
         return ;
     }
-    if(pwd == null || pwd.length < 8 || pwd.length > 16){
-        alert("密码长度错误");
-        return ;
-    }
+    
     var login = document.getElementById("loginForm");
     login.action = "/ReviewSystem/LoginServlet";
     login.submit();
